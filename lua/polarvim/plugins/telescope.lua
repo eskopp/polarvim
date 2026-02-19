f7981c1be5423a2d125235d77a2c10acde209553
eskopp <info@erik-skopp.de>
2026-02-19 11:17:11 +0100

    Modernize polarvim (Neovim 0.11 LSP + Telescope)

--- lua/polarvim/plugins/telescope.lua
+++ lua/polarvim/plugins/telescope.lua
@@ -1,12 +1,24 @@
 return {
-  { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" }, config = function()
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
       local telescope = require("telescope")
       telescope.setup({})
 
       local builtin = require("telescope.builtin")
+
       vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files" })
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep,  { desc = "Live grep" })
-      vim.keymap.set("n", "<leader>fb", builtin.buffers,    { desc = "Buffers" })
-    end
-  }
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Buffers" })
+
+      vim.keymap.set("n", "<leader>fH", function()
+        builtin.find_files({ cwd = vim.fn.expand("~") })
+      end, { desc = "Find files (home)" })
+
+      vim.keymap.set("n", "<leader>f/", function()
+        builtin.find_files({ cwd = "/" })
+      end, { desc = "Find files (/)" })
+    end,
+  },
 }
